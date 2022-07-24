657d2c6fbe47440f360e4ad9053ce80cd5899b75
Maksym <72620745+MaksimLik@users.noreply.github.com>
2022-07-24 20:35:56 +0300

    Construction for book

--- src/Book.java
+++ src/Book.java
@@ -3,5 +3,12 @@
         String author;
         int pages;
         String language;
+
+        public Book(String title, String author, int pages, String language) {
+            this.title = title;
+            this.language = language;
+            this.author = author;
+            this.pages = pages;
+        }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Book book1 = new Book();
-        book1.author = "J. Rowling";
-        book1.language = "Eng";
-        book1.pages = 101;
-        book1.title = "Harry Potter";
+        Book book1 = new Book("Harry Potter", "J. Rowling", 100, "ENG");
         System.out.println(book1.author + "\n" + book1.title + "\n" + book1.language + "\n" + book1.pages);
     }
 }
