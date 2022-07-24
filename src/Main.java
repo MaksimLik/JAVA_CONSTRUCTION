6d9e88a3c2140d354c41083a359f0c39bdc87b07
Maksym <72620745+MaksimLik@users.noreply.github.com>
2022-07-24 20:16:52 +0300

    Construction for book
    + class Book

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Book book1 = new Book();
+        book1.author = "J. Rowling";
+        book1.language = "Eng";
+        book1.pages = 101;
+        book1.title = "Harry Potter";
+        System.out.println(book1.author + "\n" + book1.title + "\n" + book1.language + "\n" + book1.pages);
     }
 }
